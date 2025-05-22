b2853fde8f142b37650989f9b629fdb342812284
Vladimir Sidorov <v.sidorov29091988@gmail.com>
2025-05-22 22:44:34 +0700

    Adds config database

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './config/configuration';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import getDataSourceConfig from './config/data-source.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, load: [configuration] }),
+    TypeOrmModule.forRoot(getDataSourceConfig()),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ backend/src/config/data-source.config.ts
@@ -0,0 +1,34 @@
+import { DataSourceOptions } from 'typeorm';
+
+export default (): DataSourceOptions => {
+  const defaultConfig = {
+    entities: [`${__dirname}/../entities/**/*{.ts,.js}`],
+    migrations: [`${__dirname}/../migrations/**/*{.ts,.js}`],
+  };
+  switch (process.env.NODE_ENV) {
+    case 'production':
+      return {
+        ...defaultConfig,
+        type: 'postgres',
+        username: process.env.DATABASE_USERNAME,
+        password: process.env.DATABASE_PASSWORD,
+        port: Number(process.env.DATABASE_PORT),
+        url: process.env.DATABASE_URL,
+        synchronize: false,
+      };
+    case 'test':
+      return {
+        ...defaultConfig,
+        type: 'sqlite',
+        database: ':memory:',
+        synchronize: true,
+      };
+    default:
+      return {
+        ...defaultConfig,
+        type: 'sqlite',
+        database: 'runit.sqlite',
+        synchronize: false,
+      };
+  }
+};
